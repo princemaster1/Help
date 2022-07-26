7ad7d9de75a62ce6a2de82cb21d6ad6a65308c08
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 16:17:20 +0300

    Update self.js

--- command/owner/self.js
+++ command/owner/self.js
@@ -2,22 +2,22 @@ module.exports = {
 	name: "self",
 	alias: ["public"],
 	category: "private",
-	desc: "Merubah self and public",
+	desc: "Switch between self and public",
 	isSpam: true,
 	isOwner: true,
 	async run({ msg, conn }, { q, map }) {
 		var command = msg.body.split(/ +/)[0].slice(1);
 		switch (command) {
 			case "public":
-				if (!map.isSelf) throw "Sudah berada dalam mode public";
+				if (!map.isSelf) throw "Already in public";
 				map.isSelf = false;
-				await msg.reply("Sukses mengubah ke mode public");
+				await msg.reply("Success change to public");
 				break;
 			case "self":
-				if (map.isSelf) throw "Sudah berada dalam mode self";
+				if (map.isSelf) throw "Already in self";
 				map.isSelf = true;
 				config.owner.push(conn.decodeJid(conn.user.id));
-				await msg.reply("Sukses mengubah ke mode self");
+				await msg.reply("Success change to self mode");
 		}
 	},
 };
