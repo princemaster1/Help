6ced8c63c4620a06773dd9e2fad82dd4ff9049cd
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 15:52:25 +0300

    Update owner.js

--- command/info/owner.js
+++ command/info/owner.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: "owner",
-	alias: ["pemilik"],
+	alias: ["host"],
 	category: "info",
 	isSpam: true,
 	async run({ msg, conn }, { q, map, args }) {
 		var msga = await conn.sendContact(msg.from, config.owner, msg);
-		await conn.sendMessage(msg.from, { text: `this my owner number, please don't spam` }, {});
+		await conn.sendMessage(msg.from, { text: `Well... He is my owner... Maybe.. 🦄` }, {});
 	},
 };
