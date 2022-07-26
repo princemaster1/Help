cc7a81050d6029cd49dff99bd3274c75af10b60c
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 15:27:15 +0300

    Update tebakbendera.js

--- command/game/tebakbendera.js
+++ command/game/tebakbendera.js
@@ -1,20 +1,20 @@
 const { cekStatus, addSesi } = require("../../lib/optiongame");
 
 module.exports = {
-	name: "tebakbendera",
+	name: "guessflag",
 	alias: ["gamebendera"],
 	category: "game",
 	desc: "Play games, guess the country flag",
 	isSpam: true,
 	isGroup: true,
-	isLimitGame: true,
+	isLimitGame: false,
 	async run({ msg, conn }, { q, map }) {
-		if (cekStatus(msg.from, map, "tebakbendera")) throw "Group Ini masih dalam sesi permainan";
+		if (cekStatus(msg.from, map, "tebakbendera")) throw "Well...";
 		let waktugame = 60;
 		let tebakbendera = await rzky.game.tebakbendera();
 		if (tebakbendera) {
 			const { key } = await msg.reply(
-				`*Game Tebak Bendera*\n\nBendera: ${tebakbendera.bendera}\nHint: ${tebakbendera.pertanyaan}\n\nAnswered Immediately, Time only 1 minute!\n\n*Happy Answering!*`
+				`*Guess The Country*\n\nFlag: ${tebakbendera.bendera}\nHint: ${tebakbendera.pertanyaan}\n\nAnswered Immediately, Time only 1 minute!\n\n*Happy Answering!*`
 			);
 			addSesi(msg.from, key.id, tebakbendera.jawaban, waktugame, map, "tebakbendera");
 			conn.game[key.id] = { status: false };
