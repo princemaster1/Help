83a99bbbd63ab1726b719acc58f2892d8e274173
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 16:07:51 +0300

    Update delprem.js

--- command/owner/delprem.js
+++ command/owner/delprem.js
@@ -3,22 +3,22 @@ module.exports = {
 	name: "delpremium",
 	alias: ["delprem", "delvip"],
 	category: "private",
-	use: "@tag / nomor",
-	query: "Penggunaan :\n*#delprem* @tag\n*#delprem* nomor",
+	use: "@tag / number",
+	query: "Example :\n*#delprem* @tag\n*#delprem* number",
 	isSpam: true,
 	isOwner: true,
 	async run({ msg, conn }, { q, map, args, arg }) {
-		if (args.length < 1) return reply(`Penggunaan :\n*#delprem* @tag\n*#delprem* nomor`);
+		if (args.length < 1) return reply(`Example :\n*#delprem* @tag\n*#delprem* number`);
 		if (msg.mentions.length !== 0) {
 			for (let i = 0; i < msg.mentions.length; i++) {
 				premium.splice(prem.getPremiumPosition(msg.mentions[i], premium), 1);
 				fs.writeFileSync("./database/premium.json", JSON.stringify(premium));
 			}
-			msg.reply("Sukses");
+			msg.reply("Done ✅");
 		} else {
 			premium.splice(prem.getPremiumPosition(args[0] + "@s.whatsapp.net", premium), 1);
 			fs.writeFileSync("./database/premium.json", JSON.stringify(premium));
-			msg.reply("sukses");
+			msg.reply("Done ✅");
 		}
 	},
 };
