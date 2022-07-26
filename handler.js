1b712acac7fa46be5ca08d2548cb7c98a3efff56
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 19:31:17 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -276,6 +276,20 @@ module.exports = handler = async (m, conn, map) => {
 				await msg.reply(teks);
 			}
 		}
+if (!isCmd ) {
+    try {
+    if (msg.quoted && msg.quoted.key.fromMe ) {
+	  if (msg.quoted.key.fromMe && /Happy\sAnswering/i.test(msg.quoted.text)) return 0;
+    if (type === 'stickerMessage') return 
+    let fetch = require('node-fetch')
+    let balasan = await fetch(`http://api.brainshop.ai/get?bid=163086&key=t78dOnZP5GQL5olm&uid=[uid]&msg=[msg]${encodeURIComponent(msg.body)}`, {method: 'get'})
+    let last = await balasan.json()
+    msg.reply(last.cnt)
+    }
+   } catch (e) {
+     console.log('Error' + e)
+   }
+ }
 
 		if (
 			!isCmd &&
