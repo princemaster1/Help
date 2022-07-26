56a1680ee2479379acfe3186ab579d374051d312
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 15:55:05 +0300

    Update scriptbot.js

--- command/info/scriptbot.js
+++ command/info/scriptbot.js
@@ -10,9 +10,9 @@ module.exports = {
 				image: { url: config.thumb },
 				footer: config.namebot,
 				// Gausah di ubah kontol najis modal copas sana sini ubah source cih
-				caption: `Script Bot Is here\ndon't forget fork + star XD`,
+				caption: `Script Bot Is here\nFollow and give star...`,
 				templateButtons: [
-					{ urlButton: { displayText: "Script Bot", url: "https://github.com/Rizky878/rzky-multidevice/" } },
+					{ urlButton: { displayText: "Bot Codes", url: "https://github.com/Fortunatusmokaya/Dreaded0" } },
 				],
 			},
 			{ quoted: msg }
