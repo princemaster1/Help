f79a271cbe0d8e1f75192068146ad7357c878927
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 16:14:09 +0300

    Update resetlimit.js

--- command/owner/resetlimit.js
+++ command/owner/resetlimit.js
@@ -1,12 +1,12 @@
 module.exports = {
 	name: "resetlimit",
 	category: "private",
-	desc: "Mereset limit",
+	desc: "resets limit",
 	isSpam: true,
 	isOwner: true,
 	async run({ msg }) {
 		limit.splice("reset");
 		require("fs").writeFileSync("./database/limit.json", JSON.stringify(limit));
-		await msg.reply(`Reset limit berhasil`);
+		await msg.reply(`Reset limit successful`);
 	},
 };
