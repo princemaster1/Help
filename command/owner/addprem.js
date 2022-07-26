37c5e58ed1e54c3ac54d80a7d8ce31f7b4bddc50
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-26 16:03:07 +0300

    Update addprem.js

--- command/owner/addprem.js
+++ command/owner/addprem.js
@@ -2,24 +2,24 @@ module.exports = {
 	name: "addpremium",
 	alias: ["addprem", "addvip"],
 	category: "private",
-	desc: "Add user premium ke database",
+	desc: "Add user into the premium database",
 	isSpam: true,
 	isOwner: true,
-	query: "Penggunaan :\n*#addprem* @tag waktu\n*#addprem* nomor waktu\n\nContoh : #addprem @tag 30d",
+	query: "Example :\n*#addprem* @tag person\n*#addprem* number\n\nExample : #addprem @tag 30d",
 	use: "@tag 30d",
 	async run({ msg, conn }, { q, map, args }) {
 		if (args.length < 2)
 			return msg.reply(
-				`Penggunaan :\n*#addprem* @tag waktu\n*#addprem* nomor waktu\n\nContoh : #addprem @tag 30d`
+				`Example :\n*#addprem* @tag someone\n*#addprem* number days\n\nExample : #addprem @tag 30d`
 			);
 		if (msg.mentions.length !== 0) {
 			for (let i = 0; i < msg.mentions.length; i++) {
 				prem.addPremiumUser(msg.mentions[0], args[1], premium);
 			}
-			msg.reply("Sukses");
+			msg.reply("Done ✅");
 		} else {
 			prem.addPremiumUser(args[1] + "@s.whatsapp.net", args[1], premium);
-			msg.reply("Sukses");
+			msg.reply("Done ✅");
 		}
 	},
 };
