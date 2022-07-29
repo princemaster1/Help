11292a7c3554c99db232242435d653d5d38a19d5
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-29 19:20:04 +0300

    Create okick.js

---
+++ command/owner/okick.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: "wasted",
+	use: "<tag>",
+	category: "group",
+	desc: "owner can kick members group",
+	wait: true,
+	isGroup: true,
+	isBotAdmin: true,
+        isOwner: true,
+        isPrefix: false,
+	isAdmin: false,
+	isSpam: true,
+	async run({ msg, conn }, { q, prefix }) {
+		let participant = msg.mentions[0]
+			? msg.mentions[0]
+			: msg.quoted
+			? msg.quoted.sender
+			: q.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+		await conn
+			.groupParticipantsUpdate(msg.from, [participant], "remove")
+			.then((res) => msg.reply("Mahn... He/She does not make sense... 👣 "))
+			.catch((err) => msg.reply(err));
+	},
+};
