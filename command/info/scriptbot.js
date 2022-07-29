e1acc1b57de6408d99355c90ceb3fb0ae9340d68
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-29 11:12:51 +0300

    Update scriptbot.js

--- command/info/scriptbot.js
+++ command/info/scriptbot.js
@@ -10,9 +10,9 @@ module.exports = {
 				image: { url: config.thumb },
 				footer: config.namebot,
 				// Gausah di ubah kontol najis modal copas sana sini ubah source cih
-				caption: `Bot Script Below\nFollow and give star...`,
+				caption: `Bot Script Below\nFollow fork and give star lol...`,
 				templateButtons: [
-					{ urlButton: { displayText: "Bot Codes", url: "https://github.com/Fortunatusmokaya/Dreaded0" } },
+					{ urlButton: { displayText: "Bot Codes", url: "https://github.com/Fortunatusmokaya/Help" } },
 				],
 			},
 			{ quoted: msg }
