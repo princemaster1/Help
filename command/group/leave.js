b17d946b27314a6f0d58b69dea88a6bf72220ace
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-07-30 18:56:41 +0300

    Update leave.js

--- command/group/leave.js
+++ command/group/leave.js
@@ -8,8 +8,8 @@ module.exports = {
   isAdmin: true,
   async run({ msg, conn }, { q }) {
   	await
-await conn.sendMessage(msg.from, {text: "Bot is now leaving group... Good bye..👋\nTo add me back use !join <group link>"})
-await require("delay")(5000);
+await conn.sendMessage(msg.from, {text: "Good bye...👋 To add me back use *!join <group link>*"})
+await require("delay")(3000);
 await conn.groupLeave(msg.from)
     },
 }
