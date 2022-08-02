920f026da1a223de74b939849c13efab40a73671
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-08-02 17:29:56 +0300

    Update runtime.js

--- command/info/runtime.js
+++ command/info/runtime.js
@@ -2,11 +2,12 @@ const { convertTime } = require("../../lib");
 
 module.exports = {
 	name: "runtime",
+        alias: "alive"
 	category: "info",
 	type: "changelog",
 	desc: "check time run  bot",
 	isSpam: true,
 	async run({ msg, conn }, { map }) {
-		await msg.reply(convertTime(map.uptime.getTime()));
+		Bot has been alive and running for await msg.reply(convertTime(map.uptime.getTime()));
 	},
 };
